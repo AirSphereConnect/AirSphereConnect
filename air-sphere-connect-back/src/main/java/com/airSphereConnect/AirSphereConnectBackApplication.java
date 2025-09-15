c166ca18a77d476d334a05eb7d24aaa5b2017271
SchCyril <schneidenbach.cyril@gmail.com>
2025-09-15 14:35:19 +0200

    push back structure

--- air-sphere-connect-back/src/main/java/com/airSphereConnect/AirSphereConnectBackApplication.java
+++ air-sphere-connect-back/src/main/java/com/airSphereConnect/AirSphereConnectBackApplication.java
@@ -1,4 +1,4 @@
-package com.airsphereconnectback;
+package com.airSphereConnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
