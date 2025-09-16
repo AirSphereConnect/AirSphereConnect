28a31a2d3b97ff0191f0cb481677429dfba22fd2
N89e <nunodafonseca@live.fr>
2025-09-16 14:15:06 +0200

    feat(CRUD users) add class users ctrl, usersService, usersRepository

---
+++ air-sphere-connect-back/src/main/java/com/airSphereConnect/controllers/UserController.java
@@ -0,0 +1,47 @@
+package com.airSphereConnect.controllers;
+
+import com.airSphereConnect.services.implementations.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Utilisateur non trouvé"));
+    }
+
+    @PostMapping
+    //Ajouter @Valid losrque spring sécurity est acctivé
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User created = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @PutMapping("/{id}")
+    //Ajouter @Valid losrque spring sécurity est acctivé
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
---
+++ air-sphere-connect-back/src/main/java/com/airSphereConnect/repositories/UserRepository.java
@@ -0,0 +1,5 @@
+package com.airSphereConnect.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> { }
