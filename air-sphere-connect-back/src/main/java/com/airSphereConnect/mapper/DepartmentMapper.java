66770243c5a3865a3061b79e461c496b55655885
N89e <nunodafonseca@live.fr>
2025-09-17 16:41:19 +0200

    feat(User) check exception

--- air-sphere-connect-back/src/main/java/com/airSphereConnect/mapper/DepartmentMapper.java
+++ air-sphere-connect-back/src/main/java/com/airSphereConnect/mapper/DepartmentMapper.java
@@ -1,30 +1,30 @@
-package com.airSphereConnect.mapper;
-
-import com.airSphereConnect.dtos.request.DepartmentRequestDto;
-import com.airSphereConnect.entities.Department;
-
-public class DepartmentMapper {
-
-
-    // RequestDto -> Entity
-    public static DepartmentRequestDto toDto(Department department) {
-        if (department == null) return null;
-
-        return new DepartmentRequestDto(
-                // Afficher l'id pas pertinent
-                department.getCode(),
-                department.getName());
-    }
-
-    // Entity -> ResponseDto
-    public static Department toEntity(DepartmentRequestDto departmentDto) {
-        if (departmentDto == null) return null;
-
-        Department department = new Department();
-        department.setCode(departmentDto.getCode());
-        department.setName(departmentDto.getName());
-
-        return department;
-    }
-
-}
+//package com.airSphereConnect.mapper;
+//
+//import com.airSphereConnect.dtos.request.DepartmentRequestDto;
+//import com.airSphereConnect.entities.Department;
+//
+//public class DepartmentMapper {
+//
+//
+//    // RequestDto -> Entity
+//    public static DepartmentRequestDto toDto(Department department) {
+//        if (department == null) return null;
+//
+//        return new DepartmentRequestDto(
+//                // Afficher l'id pas pertinent
+//                department.getCode(),
+//                department.getName());
+//    }
+//
+//    // Entity -> ResponseDto
+//    public static Department toEntity(DepartmentRequestDto departmentDto) {
+//        if (departmentDto == null) return null;
+//
+//        Department department = new Department();
+//        department.setCode(departmentDto.getCode());
+//        department.setName(departmentDto.getName());
+//
+//        return department;
+//    }
+//
+//}
