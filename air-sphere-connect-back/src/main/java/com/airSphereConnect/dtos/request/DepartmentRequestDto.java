68adbd05a9cdabf3be66718a3d5f7e1545f116a1
N89e <nunodafonseca@live.fr>
2025-09-17 18:48:47 +0200

    update

--- air-sphere-connect-back/src/main/java/com/airSphereConnect/dtos/request/DepartmentRequestDto.java
+++ air-sphere-connect-back/src/main/java/com/airSphereConnect/dtos/request/DepartmentRequestDto.java
@@ -1,31 +1,31 @@
-//package com.airSphereConnect.dtos.request;
-//
-//public class DepartmentRequestDto {
-//    // TODO implement ToDto and FromDto methods
-//    private String name;
-//    private String code;
-//
-//    public DepartmentRequestDto() {
-//    }
-//
-//    public DepartmentRequestDto(String name, String code) {
-//        this.name = name;
-//        this.code = code;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getCode() {
-//        return code;
-//    }
-//
-//    public void setCode(String code) {
-//        this.code = code;
-//    }
-//}
+package com.airSphereConnect.dtos.request;
+
+public class DepartmentRequestDto {
+    // TODO implement ToDto and FromDto methods
+    private String name;
+    private String code;
+
+    public DepartmentRequestDto() {
+    }
+
+    public DepartmentRequestDto(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
--- air-sphere-connect-back/src/main/java/com/airSphereConnect/dtos/response/DepartmentResponseDto.java
+++ air-sphere-connect-back/src/main/java/com/airSphereConnect/dtos/response/DepartmentResponseDto.java
@@ -1,63 +1,63 @@
-//package com.airSphereConnect.dtos.response;
-//
-//import java.util.Objects;
-//
-//public class DepartmentResponseDto {
-//    private Long id;
-//    private String name;
-//    private String code;
-//
-//    public DepartmentResponseDto() {
-//    }
-//
-//    public DepartmentResponseDto(Long id, String name, String code) {
-//        this.id = id;
-//        this.name = name;
-//        this.code = code;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getCode() {
-//        return code;
-//    }
-//
-//    public void setCode(String code) {
-//        this.code = code;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (o == null || getClass() != o.getClass()) return false;
-//        DepartmentResponseDto that = (DepartmentResponseDto) o;
-//        return Objects.equals(id, that.id);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hashCode(id);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "DepartmentResponseDto{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", code='" + code + '\'' +
-//                '}';
-//    }
-//}
+package com.airSphereConnect.dtos.response;
+
+import java.util.Objects;
+
+public class DepartmentResponseDto {
+    private Long id;
+    private String name;
+    private String code;
+
+    public DepartmentResponseDto() {
+    }
+
+    public DepartmentResponseDto(Long id, String name, String code) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        DepartmentResponseDto that = (DepartmentResponseDto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "DepartmentResponseDto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", code='" + code + '\'' +
+                '}';
+    }
+}
