d3b73389b93416c4c88ccb7eb037527c11e9f002
Sandrine ALCAZAR <33418628+Vanda89@users.noreply.github.com>
2025-10-19 23:32:49 +0200

    ✨ feat(app): integrate ThemeService in AppComponent to test theme toggle

--- air-sphere-connect-front/src/app/app.ts
+++ air-sphere-connect-front/src/app/app.ts
@@ -1,13 +1,28 @@
-import { Component, signal } from '@angular/core';
+import {Component, signal, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService} from './core/services/theme';
 import {Header} from './shared/components/layout/header/header';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
-export class App {
-  protected readonly title = signal('air-sphere-connect-front');
+
+export class App implements OnInit {
+  protected readonly title = signal('AirSphereConnect');
+
+  private readonly themeService = inject(ThemeService);
+
+  readonly isDarkTheme = this.themeService.isDarkMode;
+
+  ngOnInit() {
+    this.themeService.watchSystemTheme();
+  }
+
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
 }
