37655730b66f2eb284f9cf7670fc65192a543974
Sandrine ALCAZAR <33418628+Vanda89@users.noreply.github.com>
2025-10-23 11:26:32 +0200

    ✨ Feat: add click event handling with disabled/loading state check
    - Prevent clicks when disabled or loading

--- air-sphere-connect-front/src/app/shared/components/ui/button/button.ts
+++ air-sphere-connect-front/src/app/shared/components/ui/button/button.ts
@@ -78,6 +78,7 @@ export class Button {
       fullWidth: this.fullWidth,
       shape: this.shape,
       wide: this.wide,
+      //isDisabled: this.disabled || this.loading,
       class: this.class
     });
   });
@@ -104,10 +105,12 @@ export class Button {
 
 
   handleClick(event: MouseEvent): void {
-    event.stopPropagation();
+
     if (!this.disabled && !this.loading) {
       this.click.emit(event);
     }
+
+    console.log(event)
   }
 
 }
