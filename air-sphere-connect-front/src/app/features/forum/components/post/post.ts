d0ee56e0179afcc0214b68a5519ce5aec8415d4a
SchCyril <schneidenbach.cyril@gmail.com>
2025-10-24 10:21:39 +0200

    add button component like

--- air-sphere-connect-front/src/app/features/forum/components/post/post.ts
+++ air-sphere-connect-front/src/app/features/forum/components/post/post.ts
@@ -1,16 +1,30 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, input, Input, output} from '@angular/core';
 import {DatePipe} from '@angular/common';
-import { Post } from '../../../../core/models/post.model';
+import {Post} from '../../../../core/models/post.model';
+import {PostService} from '../../../../core/services/post.service';
+import {Button} from '../../../../shared/components/ui/button/button';
+import {IconComponent} from '../../../../shared/components/ui/icon/icon';
 
 @Component({
   selector: 'app-post',
+  standalone: true,
   imports: [
-    DatePipe
+    DatePipe,
+    Button,
+    IconComponent
   ],
   templateUrl: './post.html',
   styleUrls: ['./post.scss']
 })
 export class PostComponent {
 
-  @Input() post!: Post;
+  private postService = inject(PostService);
+
+  post = input.required<Post>()
+  onLike = output<number>();
+
+  onLikePost(): void {
+    this.onLike.emit(this.post().id);
+  }
+
 }
